c1dcf446d8d1eed53eb90c15a5774c6b248c31f3
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-05 12:09:07 -0300

    fix: fixing light and dark mode

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['user-images.githubusercontent.com']
+    dangerouslyAllowSVG: true,
+    domains: ['user-images.githubusercontent.com', 'api.producthunt.com']
   }
 }
 module.exports = nextConfig
