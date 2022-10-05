0c033e8fee0ff17ffcd6f6b79c672790bae2c751
jrafaaael <jrmd070102@gmail.com>
2022-10-05 17:56:47 -0400

    fix color on dark mode

--- src/components/tag/index.jsx
+++ src/components/tag/index.jsx
@@ -2,7 +2,7 @@ export default function Tag({ name }) {
   return (
     <button
       key={name}
-      className="px-2 bg-purple-200 border border-purple-400 rounded-md text-purple-600 dark:bg-purple-600 dark:border-purple-400 dark:text-purple-100"
+      className="px-2 bg-purple-200 border border-purple-400 rounded-md text-purple-600 dark:bg-transparent dark:border-purple-400 dark:text-purple-200 dark:hover:bg-purple-200 dark:hover:text-purple-600"
     >
       <small className="text-sm uppercase">{name}</small>
     </button>
