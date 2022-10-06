c4eec9724f43490b4b7f271ed3ec9f28b1a7f9cc
Vlady <contact.johngoldco@gmail.com>
2022-10-06 15:12:01 +0200

    Add fucntion to change tag style onClick

--- src/components/tag/index.jsx
+++ src/components/tag/index.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-function Tag({ name }) {
+function Tag({ name, currentTag, onCurrentTag }) {
     return (
-        <button
+        <button onClick={() => {onCurrentTag(name)}}
             type="button"
             key={name}
-            className="px-2 bg-purple-200 border border-purple-400 rounded-md text-purple-600 dark:bg-transparent dark:border-purple-400 dark:text-purple-200 dark:hover:bg-purple-200 dark:hover:text-purple-600"
+            className={`px-2 bg-purple-200 border border-purple-400 rounded-md text-purple-600 ${ currentTag === name ? 'dark:bg-purple-200 dark:text-purple-600' : 'dark:bg-transparent'} dark:border-purple-400 dark:text-purple-200 dark:hover:bg-purple-200 dark:hover:text-purple-600`}
         >
             <small className="text-sm uppercase">{name}</small>
         </button>
